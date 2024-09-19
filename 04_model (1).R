9865bd5191ba7f34d05b507329c70516148d03c2
lauragao75 <lauragao75@gmail.com>
2024-09-19 18:28:12 -0400

    Add files via upload

---
+++ 04_model (1).R
@@ -0,0 +1,23 @@
+#### Preamble ####
+# Purpose : Models
+# Author : Zien Gao
+# Date 19 September 2024
+# Contact : lauragao75@gmail.com
+# License: MIT
+# Pre-requisites: None
+# Any other information needed? None
+
+
+
+#### Workspace setup ####
+library(tidyverse)
+library(rstanarm)
+
+#### Read data ####
+analysis_data <- read_csv("analysis_data.csv")
+
+### Model data ####
+
+
+
+#### Save model ####
